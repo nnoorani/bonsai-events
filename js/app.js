07f1427073f3e4e3b8b71085009ba9f3b6f0d910
Noorain Noorani <noorain.noorani@gmail.com>
2014-04-13 00:34:25 -0400

    reformatting and adding backgrounds

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@ var s = skrollr.init({
 	forceHeight: false
 });
 
-$(window).scroll(function () {
-    if ($(window).scrollTop() >= $('#about').offset().top) {
-    	$('.navbar').addClass('navbar-fixed-top'); 
-    	$('.navbar').removeClass('hidden'); 
-    } else {
-        $('.navbar').removeClass('navbar-fixed-top');  
-        $('.navbar').addClass('hidden'); 
-    }
-});         
+// $(window).scroll(function () {
+//     if ($(window).scrollTop() >= $('#about').offset().top) {
+//     	$('.navbar').addClass('navbar-fixed-top'); 
+//     	$('.navbar').removeClass('hidden'); 
+//     } else {
+//         $('.navbar').removeClass('navbar-fixed-top');  
+//         $('.navbar').addClass('hidden'); 
+//     }
+// });         
